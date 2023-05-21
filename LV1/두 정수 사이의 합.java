b87ff84664659bea590b31b8a945a51132809040
gi0-h <103099496+gi0-h@users.noreply.github.com>
2023-05-21 16:02:35 +0900

    Update 두 정수 사이의 합.java

--- LV1/두 정수 사이의 합.java
+++ LV1/두 정수 사이의 합.java
@@ -12,3 +12,29 @@ class Solution {
         return answer;
     }
 }
+
+
+/* 참고할만한 풀이 
+//위의 코드와 유사하나 더 간결
+class Solution {
+  public long solution(int a, int b) {
+      long answer = 0;
+      for (int i = ((a < b) ? a : b); i <= ((a < b) ? b : a); i++) 
+          answer += i;
+
+      return answer;
+  }
+}
+
+//등차수열의 합 공식을 이용한 코드
+class Solution {
+
+    public long solution(int a, int b) {
+        return sumAtoB(Math.min(a, b), Math.max(b, a));
+    }
+
+    private long sumAtoB(long a, long b) {
+        return (b - a + 1) * (a + b) / 2;
+    }
+}
+*/
