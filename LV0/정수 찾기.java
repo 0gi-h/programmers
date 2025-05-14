3939c68f7149cd7abcf204fa1327ea96cf3be599
0gi-h <103099496+0gi-h@users.noreply.github.com>
2025-05-14 22:05:21 +0900

    Update 정수 찾기.java

--- LV0/정수 찾기.java
+++ LV0/정수 찾기.java
@@ -7,6 +7,7 @@ class Solution {
         String tmp = "";
         
         //스트림의 변환 메서드는 List인터페이스에서 구현되어 있기 때문에 ArrayList가 아닌 List로 선언
+        //String은 괜찮지만 기본형의 경우 asList() 불가
         List<Integer> box = Arrays.stream(num_list).boxed().collect(Collectors.toList());
         
         answer = box.contains(n) ? 1 : 0;
